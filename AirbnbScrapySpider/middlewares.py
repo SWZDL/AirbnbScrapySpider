3d4c01ad7d0cef421952d5f3ad0e1253963e7fda
Wenzeng Sheng <45939754+SWZDL@users.noreply.github.com>
2022-03-13 16:45:28 +0800

    更改了获取房屋详情界面 html 的 middleware；修复了配置文件中数据库配置

--- AirbnbScrapySpider/middlewares.py
+++ AirbnbScrapySpider/middlewares.py
@@ -25,7 +25,6 @@ class AirbnbScrapySpiderDownloaderMiddleware:
 
     def process_request(self, request, spider):
         # 如果是登录获取 cookie 的爬虫
-        global raw_html
         if request.meta.get('flag') == flags['login']:
             spider.browser.get(request.url)
             WebDriverWait(spider.browser, 300, 1).until(
@@ -50,11 +49,11 @@ class AirbnbScrapySpiderDownloaderMiddleware:
             return HtmlResponse(url=spider.browser.current_url, request=request, body="登陆完成", encoding="utf-8")
         else:
             # 如果是获取房屋详细信息的爬虫，需要额外进行切换页面操作
-            if request.meta.get('flag') == 'room_detail':
-                new_window = 'window.open("{}")'.format(request.url)
-                spider.browser.execute_script(new_window)
-                # 移动句柄，对新打开页面进行操作
-                spider.browser.switch_to_window(spider.browser.window_handles[1])
+            # if request.meta.get('flag') == 'room_detail':
+            #     new_window = 'window.open("{}")'.format(request.url)
+            #     spider.browser.execute_script(new_window)
+            #     # 移动句柄，对新打开页面进行操作
+            #     spider.browser.switch_to_window(spider.browser.window_handles[1])
 
             spider.browser.get(url=request.url)
             # 注入 cookie
@@ -81,23 +80,15 @@ class AirbnbScrapySpiderDownloaderMiddleware:
 
         if request.meta.get('flag') == flags['room_detail']:
             # 如果是获取房屋详情界面，需要额外等待，确保评论加载完成，以及需要进行新标签页的关闭和切换
-            WebDriverWait(spider.browser, 8, 1).until(
-                EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/main/div[2]/div/div/div/div[2]/div/div[1]/div/main/div[4]/div/div/div/section/div[1]/h1/div"))
+            WebDriverWait(spider.browser, 30, 1).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/main/div/div/div/div/div[2]/div/div[1]/div/div[4]/div/div/div/section"))
             )
-            element = spider.browser.find_element_by_tag_name('body')
-            element.send_keys(Keys.END)
-            raw_html = spider.browser.page_source
-            # 关闭当前标签页
-            spider.browser.close()
-            spider.browser.switch_to_window(spider.browser.window_handles[0])
 
-        elif request.meta.get('flag') == flags['homes_list']:
-            # 这里则是获取房源列表的操作
-            # 获取本页的列表
-            element = spider.browser.find_element_by_tag_name('body')
-            element.send_keys(Keys.END)
-            time.sleep(8)
-            raw_html = spider.browser.page_source
+        # 这里是 获取房源列表 和 获取房屋详情 的操作
+        element = spider.browser.find_element_by_tag_name('body')
+        element.send_keys(Keys.END)
+        time.sleep(8)
+        raw_html = spider.browser.page_source
         return HtmlResponse(url=spider.browser.current_url, body=raw_html, encoding="utf8", request=request)
 
     def process_response(self, request, response, spider):
--- AirbnbScrapySpider/spiders/roomDetailSpider.py
+++ AirbnbScrapySpider/spiders/roomDetailSpider.py
@@ -17,12 +17,6 @@ class RoomDetailSpider(scrapy.Spider):
     allowed_domains = ['airbnb.cn']
     start_urls = []
 
-    def __init__(self, name=None, **kwargs):
-        self.browser = config.getDriver()
-        self.url_list = self.get_room_url()
-        self.start_urls = [self.url_list.pop()]
-        super().__init__(name, **kwargs)
-
     def get_room_url(self):
         # 建立数据库连接
         with SSHTunnelForwarder(
@@ -44,8 +38,14 @@ class RoomDetailSpider(scrapy.Spider):
             cursor = connection.cursor()
             sql = "SELECT room_url FROM rooms where rooms.room_reviews_num is null limit 100"
             cursor.execute(sql)
-            res = cursor.fetchall()
-            return list(res)
+            res = [item[0] for item in cursor.fetchall()]
+            return res
+
+    def __init__(self, name=None, **kwargs):
+        self.browser = config.getDriver()
+        self.url_list = self.get_room_url()
+        self.start_urls = [self.url_list.pop()]
+        super().__init__(name, **kwargs)
 
     def start_requests(self):
         response = scrapy.Request(self.start_urls[0], callback=self.parse, meta={"flag": "room_detail"})
@@ -54,16 +54,22 @@ class RoomDetailSpider(scrapy.Spider):
     def parse(self, response, **kwargs):
         # parse the detail at here
         reviews_count = response.xpath("/html/body/div[3]/div/main/div[2]/div/div/div/div[2]/div/div[1]/div/main/div[4]/div/div/div/section/div[2]/div[1]/div/div/div/div/div/span/text()").extract()
+        print("=============================================")
+        print("reviews_count：{}".format(reviews_count))
+        print("=============================================")
         if reviews_count:  # 如果有评论就采集，否则跳过
             price = response.xpath("/html/body/div[3]/div/main/div/div/div/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div/div/div/div/div/div[1]/div[1]/div/span[2]/span/text()").extract()
             description = response.xpath("/html/body/div[8]/div/div/div/div/div/div/section/div/section/div/div[1]/div[2]")
             img_list_div = response.xpath('/html/body/div[3]/div/main/div/div/div/div/div[1]/div[1]/div/div/div[1]/div/div[1]')
+            print("=============================================")
             print("price:{}\ndescription:{}\nimg_list_div:{}".format(price, description, img_list_div))
+            print("=============================================")
             # yield self.item  # 将房屋信息存入数据库
             # yield scrapy.Request(url=landlord_detail_url, callback=self.parse_landlord_detail, meta={"item": item})  # 通过 参数meta 可以将item参数传递进 callback回调函数,再由 response.meta[...]取出来
         # continue to get the next one
-        if len(self.url_list) >= 1:
-            response = scrapy.Request(self.url_list.pop(), callback=self.parse, meta={"flag": "room_detail"})
-            yield response
-        else:
-            self.browser.close()
+        # if len(self.url_list) >= 1:
+        #     response = scrapy.Request(self.url_list.pop(), callback=self.parse, meta={"flag": "room_detail"})
+        #     yield response
+        # else:
+        #     self.browser.close()
+        self.browser.close()
